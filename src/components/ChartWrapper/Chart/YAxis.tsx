5454eca02a3f8e7330fe60872a41faf2abbaae84
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-06-16 16:12:50 +0530

    chore: clear readme

--- src/components/ChartWrapper/Chart/YAxis.tsx
+++ src/components/ChartWrapper/Chart/YAxis.tsx
@@ -6,10 +6,10 @@ export const YAxis = () => {
         return Math.max(maxYValue, yValue);
     }, 0);
     return (<div className="flex flex-col pb-15">
-        <div className="flex-1">{maxYValue}</div>
-        <div className="flex-1" >{maxYValue * 4/ 5}</div>
-        <div className="flex-1" >{maxYValue * 3/ 5}</div>
-        <div className="flex-1">{maxYValue * 2/ 5}</div>
-        <div className="flex-1">{maxYValue / 5}</div>
+        <div className="flex-1">{maxYValue} -</div>
+        <div className="flex-1" >{maxYValue * 4/ 5} -</div>
+        <div className="flex-1" >{maxYValue * 3/ 5} -</div>
+        <div className="flex-1">{maxYValue * 2/ 5} -</div>
+        <div className="flex-1">{maxYValue / 5} -</div>
     </div>);
 };
