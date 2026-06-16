0d875c8ad1469f1dfa56927a2a36076189464e09
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-06-16 13:38:29 +0530

    feat: remove scroll width

--- src/components/ChartWrapper/Sidebar/BarList.tsx
+++ src/components/ChartWrapper/Sidebar/BarList.tsx
@@ -4,7 +4,7 @@ export const BarList = () => {
     const { bars, deleteBar } = useBarContext();
 
     return (
-        <ul className="flex-1 flex flex-col gap-2 overflow-auto">
+        <ul className="flex-1 flex flex-col gap-2 overflow-auto scrollbar-none">
             {bars.map(({ id, xLabel, yValue }) => (
                 <li key={id} className="border-2 flex flex-col gap-2 p-2 rounded-lg">
                     <div>X-Label : {xLabel}</div>
